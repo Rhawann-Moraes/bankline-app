9854a17dca0a065fc381748ddc35c7639d5c7628
Rhawann Moraes <93962806+Rhawann-Moraes@users.noreply.github.com>
2022-05-19 22:58:09 -0300

    integrando o Front-end ao Back-end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,22 @@ import { AppComponent } from './app.component';
 import { MovimentacaoListComponent } from './components/movimentacao-list/movimentacao-list.component';
 import { MovimentacaoNewComponent } from './components/movimentacao-new/movimentacao-new.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     MovimentacaoListComponent,
     MovimentacaoNewComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
