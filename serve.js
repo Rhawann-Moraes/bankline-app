f9bd4377d7f1eede0d6cfa4dffd5ac35e8224bae
Rhawann Moraes <93962806+Rhawann-Moraes@users.noreply.github.com>
2022-05-21 15:45:39 -0300

    Preparando o Deploy para o Heroku

---
+++ serve.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const PORT
+
+= process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/dist/bankline-app'));
+
+app.get('/', (req, res) => { 
+    res.sendFile(__dirname + '/dist/bankline-app/index.html');
+});
+app.listen(PORT, () => {
+
+console.log('Servidor iniciado na porta' + PORT);
+})
